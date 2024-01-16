55f94396c5c14ae7ae386523a031c4dddf67b377
Zackery Grizzle <zagrizzl@microsoft.com>
2024-01-16 11:28:08 -0600

    init

---
+++ Assets/WheelControl.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class WheelControl : MonoBehaviour
+{
+    public Transform wheelModel;
+
+    [HideInInspector] public WheelCollider WheelCollider;
+
+    // Create properties for the CarControl script
+    // (You should enable/disable these via the 
+    // Editor Inspector window)
+    public bool steerable;
+    public bool motorized;
+
+    Vector3 position;
+    Quaternion rotation;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        WheelCollider = GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the Wheel collider's world pose values and
+        // use them to set the wheel model's position and rotation
+        WheelCollider.GetWorldPose(out position, out rotation);
+        wheelModel.transform.position = position;
+        wheelModel.transform.rotation = rotation;
+    }
+}
